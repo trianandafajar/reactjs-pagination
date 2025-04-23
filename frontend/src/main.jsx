92277ad79ffff5e016cc3d7bb7851bd9f89982f9
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-23 10:28:25 +0700

    Update Main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,12 @@ import App from "./App.jsx";
 import "bulma/css/bulma.css";
 import axios from "axios";
 
-axios.defaults.headers.post["Content-Type"] =
-  "application/x-www-form-urlencoded";
-axios.defaults.baseURL = import.meta.env.VITE_API_URL;
-axios.defaults.timeout = import.meta.env.VITE_API_TIMEOUT;
+// Axios global config
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+axios.defaults.timeout = parseInt(import.meta.env.VITE_API_TIMEOUT) || 10000;
+axios.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
 
+// Render root
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
